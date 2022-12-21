0b6ee892460b5625d46a1dd038c52f2a4b2e8148
Oscar Wikström <oscar.wikstrom@hotmail.com>
2022-12-21 14:44:31 +0100

    post och delete

---
+++ 02-todo/server/app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const fs = require("fs/promises");
+
+const PORT = 5000;
+
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Method", "*");
+
+    next();
+  });
+
+app.get("/task", async (req, res) => {
+  try {
+    const tasks = await fs.readFile("./tasks.json");
+    res.send(JSON.parse(tasks));
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+app.post("/task", async (req, res) => {
+  try {
+    const task = req.body;
+
+    const listBuffer = await fs.readFile("./tasks.json");
+    const currentTasks = JSON.parse(listBuffer);
+    let maxTaskId = 1;
+    if (currentTasks && currentTasks.length > 0) {
+      maxTaskId = currentTasks.reduce(
+        (maxId, currentElement) =>
+          currentElement.id > maxId ? currentElement.id : maxId,
+        maxTaskId
+      );
+    }
+
+    const newTask = { id: maxTaskId + 1, ...task };
+    const newList = currentTasks ? [...currentTasks, newTask] : [newTask];
+
+    await fs.writeFile("./tasks.json", JSON.stringify(newList));
+    res.send(newTask);
+  } catch (error) {
+    res.status(500).send({ error: error.stack });
+  }
+});
+
+app.delete("/task/:id", async (req, res) => {
+try {
+    const id = req.params.id;
+    const listBuffer = await fs.readFile('./tasks.json');
+    const currentTasks = JSON.parse(listBuffer)
+    if(currentTasks.length > 0){
+        await fs.writeFile('./tasks.json', JSON.stringify(currentTasks.filter((task) => task.id != id)));
+        res.send({message: `uppgift med id ${id} togs bort`});
+    }else {
+        res.status(404).send({error: "ingen uppgift att ta bort!"})
+    }
+} catch (error) {
+    res.status(500).send({ error: error.stack });
+}
+
+});
+app.listen(PORT, () => console.log("Server running on http://localhost:5000"));
